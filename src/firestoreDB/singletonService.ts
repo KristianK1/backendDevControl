f973a36b61b67639c7541d201946c02647632d74
KristianK1 <kikihd11@gmail.com>
2022-11-25 01:07:10 +0100

    added empty functions to userRightDB

--- src/firestoreDB/singletonService.ts
+++ src/firestoreDB/singletonService.ts
@@ -1,6 +1,7 @@
 import { DeviceDB } from "./devices/deviceDB";
 import { FirestoreDB } from "./firestore";
 import { getMaxIds } from "./MaxIDs/MaxIDs";
+import { UserRightsDB } from "./userRights/userRightsDB";
 import { UsersDB } from "./users/userDB";
 
 export const firestoreSingletonFactory = (function () {
@@ -74,3 +75,23 @@ export const getMaxIDSingletonFactory = (function () {
         }
     };
 })();
+
+
+export const getUserRightDBSingletonFactory = (function () {
+    var userRightsDB: UserRightsDB;
+
+    function createInstance(): UserRightsDB {
+        var object = new UserRightsDB();
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!userRightsDB) {
+                userRightsDB = createInstance();
+            }
+            return userRightsDB;
+        }
+    };
+})();
+
--- src/firestoreDB/userRights/userRightsDB.ts
+++ src/firestoreDB/userRights/userRightsDB.ts
@@ -3,8 +3,9 @@ import { IComplexFieldGroup, IComplexFieldGroupState, IDevice, IDeviceFieldBasic
 import { FirestoreDB } from '../firestore';
 import { getMaxIds } from '../MaxIDs/MaxIDs';
 import { FieldValue } from 'firebase-admin/firestore';
-import { firestoreSingletonFactory, getMaxIDSingletonFactory } from '../singletonService';
+import { firestoreSingletonFactory, getMaxIDSingletonFactory, usersDBSingletonFactory } from '../singletonService';
 import { getCurrentTimeUNIX } from '../../generalStuff/timeHandlers';
+import { getUserDBInstance, UsersDB } from 'firestoreDB/users/userDB';
 
 var deviceDBObj: UserRightsDB;
 
@@ -18,5 +19,48 @@ export function getUserRightsDBInstance(): UserRightsDB {
 
 export class UserRightsDB {
 
+    firestore: FirestoreDB;
+    getMaxIds: getMaxIds;
+    usersDB: UsersDB;
+
+    constructor() {
+        this.firestore = firestoreSingletonFactory.getInstance();
+        this.getMaxIds = getMaxIDSingletonFactory.getInstance();
+        this.usersDB = usersDBSingletonFactory.getInstance();
+    }
+
+    async addUserRightToDevice(userId: number, deviceId: number, readOnly: boolean) {
+
+    }
+
+    async deleteUserRightToDevice(userId: number, deviceId: number) {
+
+    }
+
+    async addUserRightToGroup(userId: number, deviceId: number, groupId: number, readOnly: boolean){
+
+    }
+
+    async deleteUserRightToGroup(userId: number, deviceId: number, groupId: number){
+
+    }
+
+    async addUserRightToField(userId: number, deviceId: number, groupId: number, fieldId: number, readOnly: boolean){
+
+    }
+
+    async deleteUserRightToField(userId: number, deviceId: number, groupId: number, fieldId: number){
+
+    }
+
+    async addUserRightToComplexGroup(userId: number, deviceId: number, complexGroupId: number, readOnly: boolean){
+
+    }
+
+    async deleteUserRightToComplexGroup(userId: number, deviceId: number, complexGroupId: number){
+
+    }
+
+    
 
 }

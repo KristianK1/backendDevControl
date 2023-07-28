78b29ea209c93f6f3a661eac2a14209e667aecbc
KristianK1 <kikihd11@gmail.com>
2023-07-28 02:57:41 +0200

    created userService

--- src/firestoreDB/db.ts
+++ src/firestoreDB/db.ts
@@ -41,52 +41,6 @@ export class Db {
         return user;
     }
 
-    async loginUserByCreds(username: string, password: string): Promise<ILoginResponse> {
-        var users = await this.getUsers();
-        const user = users.find(user => user.username === username);
-        if (!user) {
-            throw ({ message: 'User doesn\'t exist' });
-        }
-        if (user.password !== password) {
-            throw ({ message: 'Wrong password' });
-        }
-        let loginResponse = {} as ILoginResponse;
-        loginResponse.username = user.username;
-        loginResponse.id = user.id;
-        loginResponse.email = user.email;
-
-        const newAuthToken = uuid().replace('-', '');
-        const authToken: IAuthToken = {} as IAuthToken;
-
-        authToken.authToken = newAuthToken;
-        authToken.userId = user.id;
-        authToken.validUntil = addDaysToCurrentTime(30);
-        loginResponse.authToken = newAuthToken;
-        await this.firestore.setDocumentValue(Db.authTokenCollName, newAuthToken, authToken);
-        return loginResponse;
-    }
-
-    async getUserByToken(token: string, updateTokenTime: boolean): Promise<IUser> {
-        let authTokenDB: IAuthToken = await this.firestore.getDocumentData(Db.authTokenCollName, token);
-        if (!authTokenDB) {
-            throw ({ message: 'Couldn\'t find token' });
-        }
-        if (hasTimePASSED(authTokenDB.validUntil)) {
-            throw ({ message: 'Token expired' });
-        }
-        const user = await this.getUserbyId(authTokenDB.userId);
-        if (!user) {
-            throw ({ message: 'User doesn\'t exist' });
-        }
-
-        if (updateTokenTime) {
-            await this.firestore.updateDocumentValue(Db.authTokenCollName, token, {
-                validUntil: addDaysToCurrentTime(30)
-            });
-        }
-        return user;
-    }
-
     async getUsers(): Promise<IUser[]> {
         let users: IUser[] = await this.firestore.getCollectionData(Db.usersCollName);
         for (let user of users) {
@@ -95,55 +49,8 @@ export class Db {
         return users;
     }
 
-    async getUserbyEmail(email: string): Promise<IUser> {
-        let users: IUser[] = await this.getUsers();
-        let user = users.find(user => user.email === email);
-        if (!user) throw ({ message: 'User doesn\'t exist' });
-        return user;
-    }
-
-    async getUserbyName(username: string): Promise<IUser> {
-        let users: IUser[] = await this.getUsers();
-        let user = users.find(user => user.username === username);
-        if (!user) throw ({ message: 'User doesn\'t exist' });
-        return user;
-    }
-
-    async addUser(username: string, password: string, email: string): Promise<number> {
-        var users = await this.getUsers();
-        var sameNameUser = users.find(user => user.username === username);
-        if (sameNameUser) throw ({ message: 'User with same name exists' });
-
-        if (email) {
-            let sameEmailUser = users.find(user => user.email === email);
-            console.log(sameEmailUser);
-            if (sameEmailUser) throw ({ message: 'User with same email exists' });
-        }
-        var maxIDdoc = await this.getMaxUserId(true);
-
-        var newUser: IUser = {
-            id: maxIDdoc + 1,
-            password: password,
-            username: username,
-            email: email,
-            userRight: { rightsToDevices: [], rightsToGroups: [], rightsToFields: [], rightsToComplexGroups: [] },
-            fieldViews: [],
-        }
-
-        if (email !== "") {
-            await this.sendEmailConfirmation_registration(newUser.id, newUser.username, email);
-        }
-
+    async addUser(newUser: IUser): Promise<void> {
         await this.firestore.setDocumentValue(Db.usersCollName, `${newUser.id}`, newUser);
-        return newUser.id;
-    }
-
-    async changeUserPassword(id: number, oldP: string, newP: string) {
-        let user = await this.getUserbyId(id);
-        if (user.password !== oldP) {
-            throw ({ message: 'Wrong password' });
-        }
-        await this.firestore.updateDocumentValue(Db.usersCollName, `${id}`, { password: newP });
     }
 
     async changeUsername(id: number, username: string) {
@@ -155,16 +62,19 @@ export class Db {
         await this.firestore.updateDocumentValue(Db.usersCollName, `${id}`, user);
     }
 
-    async deleteUser(token: string) {
-        let user = await this.getUserByToken(token, false);
-        if (!user) {
-            throw ({ message: 'User doesn\'t exist in database' });
-        }
-        await this.firestore.deleteDocument(Db.usersCollName, `${user.id}`);
+    async updateUserEmail(userId: number, email: string) {
+        await this.firestore.updateDocumentValue(Db.usersCollName, `${userId}`, { email: email });
+    }
 
+    async updateUserPassword(userId: number, password: string) {
+        await this.firestore.updateDocumentValue(Db.usersCollName, `${userId}`, { password: password });
+    }
+
+    async deleteUser(userId: number) {
+        await this.firestore.deleteDocument(Db.usersCollName, `${userId}`);
         let tokens = await this.getTokens();
         tokens.forEach(async token => {
-            if (token.userId === user.id) {
+            if (token.userId === userId) {
                 await this.firestore.deleteDocument(Db.authTokenCollName, token.authToken);
             }
         });
@@ -673,6 +583,30 @@ export class Db {
 
 
     //<TOKENs>
+    async generateAndSaveNewToken(userId: number): Promise<IAuthToken> {
+
+        const newAuthToken = uuid().replace('-', '');
+        const authToken: IAuthToken = {} as IAuthToken;
+
+        authToken.authToken = newAuthToken;
+        authToken.userId = userId;
+        authToken.validUntil = addDaysToCurrentTime(30);
+
+        await this.firestore.setDocumentValue(Db.authTokenCollName, newAuthToken, authToken);
+
+        return authToken;
+    }
+
+    async getToken(token: string): Promise<IAuthToken> {
+        return await this.firestore.getDocumentData(Db.authTokenCollName, token);
+    }
+
+    async extendToken(token: string): Promise<void> {
+        await this.firestore.updateDocumentValue(Db.authTokenCollName, token, {
+            validUntil: addDaysToCurrentTime(30)
+        });
+    }
+
     async getTokens(): Promise<IAuthToken[]> {
         return await this.firestore.getCollectionData(Db.authTokenCollName);
     }
@@ -1422,111 +1356,28 @@ export class Db {
 
 
     //<EMAIL>
-
-
-    async sendEmailConfirmation_registration(id: number, username: String, email: string) {
-        let hashCode = uuid();
-        await this.emailService.sendRegistrationEmail(username, email, hashCode);
-
-        let emailConfirmationData: IEmailConfirmationData = {
-            userId: id,
-            hashCode: hashCode,
-            email: email,
-        }
-        await this.firestore.setDocumentValue(Db.emailConfirmationsCollName, hashCode, emailConfirmationData);
+    async saveEmailConfirmationData(emailConfirmationData: IEmailConfirmationData): Promise<void> {
+        await this.firestore.setDocumentValue(Db.emailConfirmationsCollName, emailConfirmationData.hashCode, emailConfirmationData);
     }
 
-    async sendEmailConfirmation_addEmail(id: number, username: String, email: string) {
-        let hashCode = uuid();
-        await this.emailService.sendAddEmailEmail(username, email, hashCode);
-
-        let emailConfirmationData: IEmailConfirmationData = {
-            userId: id,
-            hashCode: hashCode,
-            email: email,
-        }
-        let confirmations: IEmailConfirmationData[] = await this.firestore.getCollectionData(Db.emailConfirmationsCollName);
-        for (let confirmation of confirmations) {
-            if (confirmation.email === email) {
-                await this.firestore.deleteDocument(Db.emailConfirmationsCollName, confirmation.hashCode);
-            }
-        }
-        await this.firestore.setDocumentValue(Db.emailConfirmationsCollName, hashCode, emailConfirmationData);
+    async deleteEmailConfirmationData(hashCode: string): Promise<void> {
+        await this.firestore.deleteDocument(Db.emailConfirmationsCollName, hashCode);
     }
 
-    async getEmailConfirmationData(hashCode: string): Promise<IEmailConfirmationData> {
-        let data: IEmailConfirmationData[] = await this.firestore.getCollectionData(Db.emailConfirmationsCollName);
-        let findCode = data.find(o => o.hashCode === hashCode);
-        if (findCode) return findCode;
-        throw { message: 'Can\'t find email confirmation' };
+    async getAllEmailConfirmationData(): Promise<IEmailConfirmationData[]> {
+        return await this.firestore.getCollectionData(Db.emailConfirmationsCollName);
     }
 
-    async confirmEmail(hashCode: string) {
-        let data: IEmailConfirmationData[] = await this.firestore.getCollectionData(Db.emailConfirmationsCollName);
-        console.log(data);
-
-        let findCode = data.find(o => o.hashCode === hashCode);
-        if (findCode) {
-
-            let users = await this.getUsers();
-            let sameEmailUser = users.find(user => user.email === findCode?.email);
-            if (sameEmailUser) throw ({ message: 'Email was already confirmed for a different user' });
-
-            if (await this.getUserbyId(findCode.userId)) {
-                await this.firestore.updateDocumentValue(Db.usersCollName, `${findCode.userId}`, { email: findCode.email });
-                await this.firestore.deleteDocument(Db.emailConfirmationsCollName, findCode.hashCode);
-
-                for (let confirmation of data) {
-                    if (confirmation.email === findCode.email) {
-                        await this.firestore.deleteDocument(Db.emailConfirmationsCollName, confirmation.hashCode);
-                    }
-                }
-            }
-            else {
-                throw ({ message: 'User doesn\'t exist' });
-            }
-        }
-        else {
-            throw ({ message: 'Invalid email confirmation code' });
-        }
-    }
-
-    async createForgotPasswordRequest(userId: number, username: string, email: string) {
-        let request: IForgotPasswordData = {
-            userId: userId,
-            hashCode: uuid(),
-            timeStamp: getCurrentTimeISO(),
-        }
-        await this.emailService.sendForgotPasswordEmail(username, email, request.hashCode);
+    async saveForgotPasswordRequest(request: IForgotPasswordData): Promise<void> {
         await this.firestore.setDocumentValue(Db.forgetPasswordRequestsCollName, `${request.userId}`, request);
     }
 
-    async getForgotPasswordRequest(hashCode: string): Promise<IForgotPasswordData> {
-        let datas: IForgotPasswordData[] = await this.firestore.getCollectionData(Db.forgetPasswordRequestsCollName);
-        let data = datas.find(o => o.hashCode === hashCode);
-        if (data == null) {
-            throw ({ message: 'Can\'t find it' });
-        }
-        return data;
+    async deleteForgotPasswordRequest(userId: number): Promise<void>{
+        await this.firestore.deleteDocument(Db.forgetPasswordRequestsCollName, `${userId}`);
     }
-
-    async changePasswordViaForgetPasswordRequest(hashCode: string, newPassword: string) {
-        let reqs: IForgotPasswordData[] = await this.firestore.getCollectionData(Db.forgetPasswordRequestsCollName);
-        let req = reqs.find(o => o.hashCode === hashCode);
-        if (req == null) {
-            throw ({ message: "Can't find the change password request" });
-        }
-
-        if (getCurrentTimeUNIX() - ISOToUNIX(req.timeStamp) > 1000 * 60 * 15) { //15 minutes
-            throw ({ message: "Expired" });
-        }
-
-        await this.firestore.updateDocumentValue(Db.usersCollName, `${req.userId}`, { password: newPassword });
-        await this.firestore.deleteDocument(Db.forgetPasswordRequestsCollName, `${req.userId}`);
+    async getAllForgotPasswordRequests(): Promise<IForgotPasswordData[]> {
+        return await this.firestore.getCollectionData(Db.forgetPasswordRequestsCollName);
     }
-
-
-
     //</EMAIL>
 
     // <MAX_IDs>
@@ -1541,7 +1392,6 @@ export class Db {
 
     async getMaxFieldId(autoIncrement: boolean): Promise<number> { return await this.getMax(Db.maxId_fieldKey, autoIncrement) }
     async setMaxFieldId(id: number) { await this.setMax(Db.maxId_fieldKey, id) }
-
 
     private async getMax(key: string, autoIncrement?: boolean): Promise<number> {
         let maxId: number;
@@ -1560,6 +1410,5 @@ export class Db {
     private async setMax(key: string, id: number) {
         return await this.firestore.setDocumentValue(Db.maxIDsCollName, key, { max: id });
     }
-
     // </MAX_IDs>
 }
